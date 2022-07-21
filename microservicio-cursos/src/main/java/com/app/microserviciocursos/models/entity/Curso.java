6a8fa493428aa4c9bcbf4151087710f68d923149
Juan Carlos Camano <jucahe27@gmail.com>
2022-07-21 15:14:37 -0500

    se crea el entity curso

---
+++ microservicio-cursos/src/main/java/com/app/microserviciocursos/models/entity/Curso.java
@@ -0,0 +1,58 @@
+package com.app.microserviciocursos.models.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "cursos")
+public class Curso {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nombre;
+
+	@Column(name = "fecha_creacion")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date fechaCreacion;
+
+	@PrePersist
+	public void creacionSistema() {
+		this.fechaCreacion = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+}
