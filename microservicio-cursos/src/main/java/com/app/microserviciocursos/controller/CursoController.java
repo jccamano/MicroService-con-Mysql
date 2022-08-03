8e021d4a5719e781668acf01d2e449f97291fcae
Juan Carlos Camano <jucahe27@gmail.com>
2022-08-03 18:58:11 -0500

    se modifican varias clases del proyecto

--- microservicio-cursos/src/main/java/com/app/microserviciocursos/controller/CursoController.java
+++ microservicio-cursos/src/main/java/com/app/microserviciocursos/controller/CursoController.java
@@ -1,13 +1,21 @@
 package com.app.microserviciocursos.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.microserviciocursos.models.entity.CursoEntity;
 import com.app.microserviciocursos.service.CursoService;
-
 
 
 @RestController
@@ -21,5 +29,41 @@ public class CursoController {
 	public ResponseEntity<?> getAllCourses(){
 		return ResponseEntity.ok().body(service.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> getCourseById (@PathVariable Long id){
+		Optional<CursoEntity> op = service.findById(id);
+		
+		if(op.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(op.get());
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> createCourse(@RequestBody CursoEntity curso){
+		CursoEntity cursoDB = service.save(curso);
+		return ResponseEntity.status(HttpStatus.CREATED).body(cursoDB);		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> editCourses(@RequestBody CursoEntity curso, @PathVariable Long id){
+		Optional<CursoEntity> op = service.findById(id);
+		if(op.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		CursoEntity cursoDB = op.get();
+		cursoDB.setNombre(curso.getNombre());
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(cursoDB));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteCourses(@PathVariable Long id){
+		service.deleteById(id);
+		return ResponseEntity.noContent().build(); 
+	}
+	
+	
 
 }
--- microservicio-cursos/src/main/java/com/app/microserviciocursos/models/entity/CursoEntity.java
+++ microservicio-cursos/src/main/java/com/app/microserviciocursos/models/entity/CursoEntity.java
@@ -1,12 +1,15 @@
 package com.app.microserviciocursos.models.entity;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -25,6 +28,10 @@ public class CursoEntity {
 	@Column(name = "fecha_creacion")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date fechaCreacion;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	//private List<AlumnoEntity> alumnos;
+	
 	
 	@PrePersist
 	public void fechaCreacion() {
